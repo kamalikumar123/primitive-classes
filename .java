00a1d1d964613c0d23e8ef0cdc7a266349b9b0be
kamali <93373518+kamalikumar123@users.noreply.github.com>
2021-12-18 15:00:25 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,11 @@
+public class ThePrimitiveClasses {
+
+    public static void main(String[] args) {
+        String s = "string";
+        
+        
+        char c = 'C';
+        System.out.println(Character.isLowerCase(c));
+    }
+    
+}
